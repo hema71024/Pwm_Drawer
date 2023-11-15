12a42be37be2f9e8deb423f8a991c39cb3adf5eb
hema71024 <56462439+hema71024@users.noreply.github.com>
2023-11-15 07:51:43 +0200

    Delete final1 directory

--- final1/final1/APP/ICU/ICU_Lconfig.c
+++
@@ -1,9 +0,0 @@
-#include "ICU.h"
-
-const ICU_ConfigurationType gStrICU_Config = 
-{
-	ICU_TIMER_1,
-	ICU_PRESCALE_64,
-	ICU_TRIGGER_FALLING_EDGE,
-	ICU_INTERRUPT_DISABLED
-};
--- final1/final1/Debug/APP/ICU/ICU_Lconfig.d
+++
@@ -1,9 +0,0 @@
-APP/ICU/ICU_Lconfig.d APP/ICU/ICU_Lconfig.o: ../APP/ICU/ICU_Lconfig.c \
- ../APP/ICU/ICU.h ../LIB/../lib/ATmega32A_Config.h \
- ../LIB/../lib/Std_Types.h
-
-../APP/ICU/ICU.h:
-
-../LIB/../lib/ATmega32A_Config.h:
-
-../LIB/../lib/Std_Types.h:
